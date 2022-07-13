e260bffb7567cb8a4d37ce4ab10c1d538f8f6ccc
szymon <sbalcerzak0@gmail.com>
2022-07-14 01:33:06 +0200

    displaying tasks

--- todo/src/components/Task.js
+++ todo/src/components/Task.js
@@ -1,13 +1,13 @@
 import TaskStyle from "./Task.module.css"
 
-const Task = ({children}) => {
+const Task = ({text, del}) => {
     return(
         <div className={TaskStyle.container}>
             <input type="checkbox" className={TaskStyle.checkbox} />
             <div className={TaskStyle.name}>
-                {children}
+                {text}
             </div>
-            <button className={TaskStyle.button}>
+            <button className={TaskStyle.button} onClick={del}>
                 delete
             </button>
         </div>
--- todo/src/components/TaskList.js
+++ todo/src/components/TaskList.js
@@ -5,7 +5,7 @@ import TaskListStyle from "./TaskList.module.css"
 const getLocalStorageData = () => {
     const data = localStorage.getItem('tasks')
     if(data){
-        return data
+        return JSON.parse(data)
     }
     else{
         return []
@@ -13,35 +13,42 @@ const getLocalStorageData = () => {
 }
 
 const TaskList = () => {
-    const [task, setTask] = useState("")
+    const [newTask, setNewTask] = useState("")
     const [tasks, setTasks] = useState(getLocalStorageData())
 
     const handleChange = (e) => {
-        setTask(e.target.value)
+        setNewTask(e.target.value)
     }
 
-    const handleClick = () => {
+    const addBtn = () => {
         setTasks([
-            ...tasks, task
+            ...tasks, newTask
         ])
+        setNewTask("")
+    }
+
+    const delBtn = (id) => {
+        const filteredTasks = tasks.filter((value, key) => (
+            key !== id
+        ))
+        setTasks(filteredTasks)
     }
 
     useEffect(() => {
-        localStorage.setItem('tasks', tasks)
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+        console.log(tasks)
     },[tasks])
-
-    console.log(tasks)
 
     return(
         <div className={TaskListStyle.container}>
             <div className={TaskListStyle.wrapper}>
                 {
-                    tasks.map((arg) => {
-                        <Task>{arg}</Task>
+                    tasks.map((value, key) => {
+                        return <Task text={value} del={() => delBtn(key)} key={key} />
                     })
                 }
-                <input type="text" value={task} onChange={e => handleChange(e)} />
-                <button onClick={() => handleClick()}>add</button>
+                <input type="text" value={newTask} onChange={e => handleChange(e)} />
+                <button onClick={() => addBtn()}>add</button>
             </div>
         </div>
     )
